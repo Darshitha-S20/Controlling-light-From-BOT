5d35bc162237a7eeeaffda8d7bb975ebe3a0e9b6
Darshitha-S20 <71258149+Darshitha-S20@users.noreply.github.com>
2020-09-14 18:50:59 +0530

    Update telebot-Darshitha.py

--- telebot-Darshitha.py
+++ telebot-Darshitha.py
@@ -1,6 +1,6 @@
 !pip install adafruit-io
-x ="darshitha "  #ENTER ADAFRUIT_IO_USERNAME
-y = " aio_DYem65E3QB4rmmdHm126gJzuWxn6"  #ENTER ADAFRUIT_IO_KEY
+x =" "  #ENTER ADAFRUIT_IO_USERNAME
+y = " "  #ENTER ADAFRUIT_IO_KEY
 from Adafruit_IO import Client, Feed
 aio = Client(x,y)
 result = aio.create_feed(new)
@@ -18,7 +18,7 @@ def off(bot,update):
   aio.create_data('bot',Data(value = 0))
   bot.send_message(chat_id =chat_id,text ="Lights Off")
 
-updater = Updater('1128882307:AAH4LbP8GZTu0oXKDdTLUWW4tpr79ic3I-0')     #Use Telegram Token HTTP API
+updater = Updater('')     #Use Telegram Token HTTP API
 dp =updater.dispatcher
 dp.add_handler(CommandHandler('on',on))
 dp.add_handler(CommandHandler('off',off))
